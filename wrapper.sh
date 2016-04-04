10a9006eff9c242ac341635c7e7f17f47ad73868
Chris Warburton <chriswarbo@gmail.com>
2016-04-04 15:32:30 +0100

    Avoid permission issues with /dev/stderr

--- wrapper.sh
+++ wrapper.sh
@@ -6,7 +6,7 @@ CMD="$1"
 
 function debugMsg {
     # Debug output, if requested
-    [[ -z "$NIX_EVAL_DEBUG" ]] || echo -e "nix-eval: $1" >> /dev/stderr
+    [[ -z "$NIX_EVAL_DEBUG" ]] || echo -e "nix-eval: $1" 1>&2
 }
 
 # The PATH may not be set up correctly, which we work around by finding the bin
@@ -18,11 +18,11 @@ NAME="$2"
 debugMsg "Looking for Haskell environment '$NAME'"
 
 DIR=$(echo "$PATH" | grep -o -- "[^:]*${NAME}/[^:]*") || {
-    echo "Couldn't find $NAME in PATH: $PATH" >> /dev/stderr
+    echo "Couldn't find $NAME in PATH: $PATH" 1>&2
     exit 1
 }
 
-debugMsg "Moving '$DIR' to the front of PATH" >> /dev/stderr
+debugMsg "Moving '$DIR' to the front of PATH" 1>&2
 export PATH=$DIR:$PATH
 
 # Now we can run the given command

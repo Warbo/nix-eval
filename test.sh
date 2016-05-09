fb8cb3628a02508cfe56b6d666eab9b79426efc1
Chris Warburton <chriswarbo@gmail.com>
2016-05-09 16:01:35 +0100

    Test debug

---
+++ test.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+
+function msg {
+    echo -e "$*" 1>&2
+}
+
+function fail {
+    msg "FAIL: $*"
+}
+
+function interpret {
+    cabal repl -v0 --ghc-option=-XOverloadedStrings
+}
+
+function expression {
+    EXPR='("head" $$ (("(:)" $$ ("show" $$ "True")) $$ "[]"))'
+    PKGS="(withPkgs [\"pandoc\"] $EXPR)"
+    if [[ PKG -eq 1 ]]
+    then
+        echo "eval $PKGS"
+    else
+        echo "eval $EXPR"
+    fi
+}
+
+function run {
+    expression | interpret
+}
+
+function getStdout {
+    run 2>/dev/null
+}
+
+function getStderr {
+    run 2>&1 1>/dev/null
+}
+
+# Tests
+
+function testBuildable {
+    MSG="Building nix-eval"
+    if OUTPUT=$(cabal build 2>&1)
+    then
+        echo "ok - $MSG"
+    else
+        echo "$OUTPUT"
+        echo "not ok - $MSG"
+        return 1
+    fi
+}
+
+function testHaveDataDir {
+    MSG="Have data dir '$nix_eval_datadir'"
+    if [[ -e "$nix_eval_datadir" ]]
+    then
+        echo "ok - $MSG"
+    else
+        echo "not ok - $MSG"
+        return 1
+    fi
+}
+
+function testEval {
+    MSG="Checking we can evaluate expressions (PKG=$PKG)"
+    OUTPUT=$(getStdout)
+    if [[ "x$OUTPUT" = 'xJust "True"' ]]
+    then
+        echo "ok - $MSG"
+    else
+        fail "Unexpected output '$OUTPUT'"
+        echo "not ok - $MSG"
+        return 1
+    fi
+}
+
+function testDebug {
+    MSG="Checking nix-eval gives debug info (PKG=$PKG)"
+    OUTPUT=$(getStderr)
+    if echo "$OUTPUT" | grep "^nix-eval: Evaluating:" > /dev/null
+    then
+        echo "ok - $MSG"
+    else
+        echo "No debug info on stderr: '$OUTPUT'"
+        echo "not ok - $MSG"
+        return 1
+    fi
+}
+
+function testIndent {
+    msg "Checking expressions get indented"
+    OUTPUT=$(getStderr)
+    echo "Indent output: $OUTPUT"
+}
+
+# Invocation
+
+function testPreconditions {
+    testBuildable && testHaveDataDir
+}
+
+function runTests {
+    testEval
+    testDebug
+    testIndent
+}
+
+export NIX_EVAL_DEBUG=1
+export nix_eval_datadir="$(dirname "$(readlink -f "$0")")"
+
+# If these fail, we bail out
+testPreconditions || exit 1
+
+msg "Testing expression without external dependencies"
+export PKG=0
+runTests
+
+msg "Testing expression with external dependencies"
+export PKG=1
+runTests

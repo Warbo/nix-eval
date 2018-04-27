354dfb8fbc72d8e35e4b528abd51d477dedc260b
Chris Warburton <chriswarbo@gmail.com>
2018-04-27 14:01:36 +0100

    Make release.nix quickly eval to a derivation defining a nix file
    This way, we can eval release.nix without too much work going on. Building the
    resulting derivation will take time, but that's fine since it's at build time,
    and it has nicely laid out dependencies, etc. The result is a .nix file which
    defines nix-eval builds for various versions of nixpkgs and GHC. We can use this
    as a build input for Hydra, which will then build all of these versions.

--- release.nix
+++ release.nix
@@ -1,45 +1,33 @@
 # Used for building and testing on Hydra
 
 # Provides a bunch of nixpkgs versions, augmented with useful helper functions
-with {
-  unstablePkgs = import ((import <nixpkgs> { config = {}; }).fetchgit {
-    url    = http://chriswarbo.net/git/nix-config.git;
-    rev    = "7b96c3a";
-    sha256 = "0sqhf599ll2qshbnmspni5r3h5mfwbbkyz5xhfb3jxhflba594zw";
-  }) {};
-};
-
 with builtins;
-with unstablePkgs.nixpkgs1709.lib;
 with rec {
-  testScript = pkgs: pkgs.runCommand "nix-eval-test-script"
-    {
-      src = ./.;
-    }
-  ''
-    mkdir "$out"
-    echo "FIXME: Skipping test script" 1>&2
-    #"$src/test.sh" 2> >(tee "$out/stderr" >&2) | tee "$out/stdout"
-  '';
+  pkgSrc =
+    with tryEval <nix-config>;
+    if success
+       then value
+       else (import <nixpkgs> { config = {}; }).fetchgit {
+              url    = http://chriswarbo.net/git/nix-config.git;
+              rev    = "e409eae";
+              sha256 = "1d4x14vv3xi53ib5ggyz2b4scj50smbkcnggzwy8z3zn3nym4pa4";
+            };
 
-  withTests = pkgs: mapAttrs (_: pkgs.withDeps [ (testScript pkgs) ]);
-
-  # "self" is a customised nixpkgs set, "super" is the corresponding original
-  buildForNixpkgs = self: super: mapAttrs (_: buildForHaskell self)
-                                          super.haskell.packages;
-
-  buildForHaskell = pkgs: hsPkgs: withTests pkgs {
-    # Uses Haskell package set provided by nixpkgs
-    nixpkgsDeps  = hsPkgs.callPackage (pkgs.runCabal2nix { url = ./.; }) {};
-
-    # Uses a Cabal sandbox to pick dependencies from (a snapshot of) Hackage
-    hackageDeps = pkgs.haskellPkgWithDeps {
-      inherit hsPkgs;
-      delay-failure = true;
-      dir           = ./.;
-    };
-  };
+  pkgs = import pkgSrc {};
 };
 
-mapAttrs (name: self: buildForNixpkgs self (getAttr name unstablePkgs))
-         unstablePkgs.customised
+with pkgs.lib;
+{
+  release = pkgs.haskellRelease {
+    dir         = ./.;
+    haskellKeep = hsVersion: !(hasPrefix "ghcjs"          hsVersion ||
+                               hasPrefix "lts"            hsVersion ||
+                               hasPrefix "ghc6"           hsVersion ||
+                               hasPrefix "ghc72"          hsVersion ||
+                               hasPrefix "ghcHaLVM"       hsVersion ||
+                               hasPrefix "integer-simple" hsVersion ||
+                               hasPrefix "ghcCross"       hsVersion ||
+                               hasSuffix "HEAD"           hsVersion);
+    nixKeep     = nixVersion: compareVersions nixVersion "nixpkgs1709" != -1;
+  };
+}

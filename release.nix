18c67cae93d7b81f3e27b0bd14d181850c009e6f
Chris Warburton <chriswarbo@gmail.com>
2018-07-13 23:34:09 +0100

    Use new nix-helpers instead of nix-config

--- release.nix
+++ release.nix
@@ -2,33 +2,18 @@
 
 # Provides a bunch of nixpkgs versions, augmented with useful helper functions
 with builtins;
-with rec {
-  pkgSrc =
-    with tryEval <nix-config>;
-    if success
-       then value
-       else (import <nixpkgs> { config = {}; }).fetchgit {
-              url    = http://chriswarbo.net/git/nix-config.git;
-              rev    = "e409eae";
-              sha256 = "1d4x14vv3xi53ib5ggyz2b4scj50smbkcnggzwy8z3zn3nym4pa4";
-            };
-
-  pkgs = import pkgSrc {};
+with {
+  helpersSrc = (import <nixpkgs> {}).fetchgit {
+    url    = http://chriswarbo.net/git/nix-helpers.git;
+    rev    = "42156fb";
+    sha256 = "0pc88y1gbli2f8f54yg070ghpm4r1j3mkj6xkmphj189s9lsv1f7";
+  };
 };
 
-with pkgs.lib;
-{
-  release = pkgs.haskellRelease {
-    name        = "nix-eval";
-    dir         = ./.;
-    haskellKeep = hsVersion: !(hasPrefix "ghcjs"          hsVersion ||
-                               hasPrefix "lts"            hsVersion ||
-                               hasPrefix "ghc6"           hsVersion ||
-                               hasPrefix "ghc72"          hsVersion ||
-                               hasPrefix "ghcHaLVM"       hsVersion ||
-                               hasPrefix "integer-simple" hsVersion ||
-                               hasPrefix "ghcCross"       hsVersion ||
-                               hasSuffix "HEAD"           hsVersion);
-    nixKeep     = nixVersion: compareVersions nixVersion "nixpkgs1709" != -1;
-  };
-}
+with import helpersSrc;
+collapseAttrs (haskellRelease {
+  name            = "nix-eval";
+  dir             = ./.;
+  haskellVersions = [ "ghc7103" ];
+  nixpkgsVersions = [ "nixpkgs1709" "nixpkgs1803" ];
+})
